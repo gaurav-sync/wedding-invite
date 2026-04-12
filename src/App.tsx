4fabe4cb07f34fc158f14248d256d38df4224e6f
Gaurav Sapkal <gauravsapkal1997@gmail.com>
2026-04-12 15:46:27 +0530

    gg

--- src/App.tsx
+++ src/App.tsx
@@ -211,7 +211,7 @@ function App() {
   const scrollToContent = () => {
     const eventsSection = document.getElementById('events');
     if (eventsSection) {
-      const top = eventsSection.getBoundingClientRect().top + window.scrollY - 40;
+      const top = eventsSection.getBoundingClientRect().top + window.scrollY - 80;
       window.scrollTo({ top, behavior: 'smooth' });
     }
   };
